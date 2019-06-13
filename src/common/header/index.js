6ab20e29e8a66f7bdc89cf3009754fa562f59993
淋酱 <3127148186@qq.com>
2019-06-13 16:41:43 +0800

    redex/react-redux实现状态管理

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,60 +1,67 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux'
 import { CSSTransition } from 'react-transition-group';
 import { HeaderWrapper, Logo, Nav, NavItem, SearchWrapper, NavSearch, Addition, Button } from './style';
+import { FOCUS_FALSE, FOCUS_TRUE  } from '../../store/actionTypes';
 
-class Header extends Component {
-  
-  constructor (props) {
-    super(props)
-    this.state = { 
-      focused: false
-    }
-    this.handleInputFocus = this.handleInputFocus.bind(this)
-    this.handleInputBlur = this.handleInputBlur.bind(this)
-  }
+// 无状态组件
+const Header = (props) => {
+  const { focused } = props
+  return ( 
+    <HeaderWrapper className='clearfix'>
+      <Logo href='/' className='fl' />
+      <Nav className='fl clearfix'>
+        <NavItem className='fl active'>首页</NavItem>
+        <NavItem className='fl'>下载app</NavItem>
+        <SearchWrapper className='fl'>
+          <CSSTransition
+            in={focused}
+            timeout={500}>
+            <NavSearch
+              className={focused ? 'focused' : ''}
+              onFocus={props.handleInputFocus}
+              onBlur={props.handleInputBlur}
+            ></NavSearch>
+          </CSSTransition>
+          <i className={focused ? 'iconfont zoom focused' : 'iconfont zoom'}>&#58900;</i>
+        </SearchWrapper>
+        <NavItem className='fr'>登录</NavItem>
+        <NavItem className='fr'>
+          <i className='iconfont'>&#58934;</i>
+        </NavItem>
+      </Nav>
+      <Addition className='clearfix'>
+        <Button className='fr writting'>
+          <i className='iconfont'>&#58901;</i>
+          <span> 写文章</span>
+        </Button>
+        <Button className='fr reg'>注册</Button>
+      </Addition>
+    </HeaderWrapper> 
+  )
+}
 
-  render() { 
-    return ( 
-      <HeaderWrapper className='clearfix'>
-        <Logo href='/' className='fl' />
-        <Nav className='fl clearfix'>
-          <NavItem className='fl active'>首页</NavItem>
-          <NavItem className='fl'>下载app</NavItem>
-          <SearchWrapper className='fl'>
-            <CSSTransition
-              in={this.state.focused}
-              timeout={500}>
-              <NavSearch
-                className={this.state.focused ? 'focused' : ''}
-                onFocus={this.handleInputFocus}
-                onBlur={this.handleInputBlur}
-              ></NavSearch>
-            </CSSTransition>
-            <i className={this.state.focused ? 'iconfont zoom focused' : 'iconfont zoom'}>&#58900;</i>
-          </SearchWrapper>
-          <NavItem className='fr'>登录</NavItem>
-          <NavItem className='fr'>
-            <i className='iconfont'>&#58934;</i>
-          </NavItem>
-        </Nav>
-        <Addition className='clearfix'>
-          <Button className='fr writting'>
-            <i className='iconfont'>&#58901;</i>
-            <span> 写文章</span>
-          </Button>
-          <Button className='fr reg'>注册</Button>
-        </Addition>
-      </HeaderWrapper> 
-    );
-  }
-
-  handleInputFocus () {
-    this.setState({focused : true})
-  }
-
-  handleInputBlur () {
-    this.setState({focused : false})
+const mapStateToProps = (state) => {
+  return {
+    focused : state.focused
   }
 }
- 
-export default Header;
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputBlur () {
+      const action = {
+        type: FOCUS_TRUE
+      }
+      dispatch(action)
+    },
+    handleInputFocus () {
+      const action = {
+        type: FOCUS_FALSE
+      }
+      dispatch(action)
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
---
+++ src/store/actionTypes.js
@@ -0,0 +1,2 @@
+export const FOCUS_FALSE = 'focus-false'
+export const FOCUS_TRUE = 'focus-true'
